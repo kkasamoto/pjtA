7cbd693a3b064072fbb85e6bc395bbc6fa5b561c
dokugakun <dokugakun.k789@outlook.jp>
2020-07-18 13:38:49 +0900

    add requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="pjtA",
     version="0.0.1",
-    install_requires=[],
+    install_requires=['requests'],
     url="https://github.com/kkasamoto/pjtA"
 )
