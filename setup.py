be7eb007ed70e8ecc615bee97752883d42ec44ce
dokugakun <dokugakun.k789@outlook.jp>
2020-07-18 13:41:16 +0900

    add package section in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pjtA",
     version="0.0.1",
     install_requires=['requests'],
+    packages=find_packages(),
     url="https://github.com/kkasamoto/pjtA"
 )
