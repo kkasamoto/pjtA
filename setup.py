36827cb561d099671d80adf9eb02e3a2241e5ae7
dokugakun <dokugakun.k789@outlook.jp>
2020-07-18 14:06:09 +0900

    change

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pjtA",
     version="0.0.1",
-    install_requires=['requests'],
     packages=['myapp'],
     url="https://github.com/kkasamoto/pjtA"
 )
